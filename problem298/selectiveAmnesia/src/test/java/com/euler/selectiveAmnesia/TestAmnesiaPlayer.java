2af15ab405303cd6a6de1be62d76a3dfe746d97e
Kamil Niemczyk <kamil.niemczyk@capgemini.com>
2015-08-11 11:30:52 +0200

    Finished

--- problem298/selectiveAmnesia/src/test/java/com/euler/selectiveAmnesia/TestAmnesiaPlayer.java
+++ problem298/selectiveAmnesia/src/test/java/com/euler/selectiveAmnesia/TestAmnesiaPlayer.java
@@ -15,18 +15,7 @@ public class TestAmnesiaPlayer {
 		player = new AmnesiaPlayer(new RobinStrategy());
 	}
 	
-	/*********** MEMORY ACCESS ***********/
-	@Test
-	public void testMemoryCannotBeAlteredExplicitly() {
-		// when
-		player.memory().add(1);
-		// then
-		assertEquals(5, player.memory().freeMemory());
-	}
-	
-	
 	/*********** MEMORY SIZE ***********/
-	
 	@Test
 	public void testWholeMemoryFreeAtBeginning() {
 		// then
@@ -44,8 +33,16 @@ public class TestAmnesiaPlayer {
 		assertEquals(0, player.memory().freeMemory());
 	}
 
+	/*********** MEMORY ACCESS ***********/
+	@Test
+	public void testMemoryCannotBeAlteredExplicitly() {
+		// when
+		player.memory().add(1);
+		// then
+		assertEquals(PlayerMemory.MEMORY_SIZE, player.memory().freeMemory());
+	}
+
 	/*********** SCORE COUNTING ***********/
-	
 	@Test
 	public void testScoreShouldBe0AtBeginning() {
 		// then
